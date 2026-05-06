8d8ec19a94dc0f64a43dde27a85b05897e3cdd27
REPENTANCE-BAEee <3544960181@qq.com>
2026-05-06 11:07:01 +0800

    1
    1

--- src/main/java/ie/ucd/monopolydeal/model/Player.java
+++ src/main/java/ie/ucd/monopolydeal/model/Player.java
@@ -11,41 +11,56 @@ public class Player {
     private final int number;
     private final List<Card> cardsAtHand = new ArrayList<>();
     private final List<Card> cardsAtBank = new ArrayList<>();
+
     public Player(String name, int number) {
         this.name = name;
         this.number = number;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public int getNumber(){
+    public int getNumber() {
         return number;
     }
 
-    public List<Card> getCardsAtHand(){
+    public List<Card> getCardsAtHand() {
         return cardsAtHand;
     }
 
-    public List<Card> getCardsAtBank(){
+    public List<Card> getCardsAtBank() {
         return cardsAtBank;
     }
 
-    public void addCardToHand(Card card){
-        if(card == null){
-            return;
-        }else{
-            cardsAtHand.add(card);
-        }
-    }
-    public void addCardToBank(Card card){
-        if(card == null){
-            return;
-        }else{
-            cardsAtHand.remove(card);
-            cardsAtBank.add(card);
-        }
+    public void addCardToHand(Card card) {
+        if (card == null) return;
+        cardsAtHand.add(card);
     }
 
+    public void addCardToBank(Card card) {
+        if (card == null) return;
+        cardsAtHand.remove(card);
+        cardsAtBank.add(card);
+    }
+
+    public boolean removeCardFromHand(Card card) {
+        return cardsAtHand.remove(card);
+    }
+
+    public boolean removeCardFromBank(Card card) {
+        return cardsAtBank.remove(card);
+    }
+
+    public int getBankTotalValue() {
+        int total = 0;
+        for (Card card : cardsAtBank) {
+            total += card.getBankValue();
+        }
+        return total;
+    }
+
+    public boolean isHandFull() {
+        return cardsAtHand.size() >= MAX_CARDS_AT_HAND;
+    }
 }
--- src/main/java/ie/ucd/monopolydeal/model/PropertySet.java
+++ src/main/java/ie/ucd/monopolydeal/model/PropertySet.java
@@ -1,4 +1,46 @@
 package ie.ucd.monopolydeal.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PropertySet {
+    private final String color;
+    private final int requiredCount;
+    private final List<Card> cards;
+
+    public PropertySet(String color, int requiredCount) {
+        this.color = color;
+        this.requiredCount = requiredCount;
+        this.cards = new ArrayList<>();
+    }
+
+    public boolean addCard(Card card) {
+        if (card == null) return false;
+        if (cards.size() >= requiredCount) return false;
+        return cards.add(card);
+    }
+
+    public boolean removeCard(Card card) {
+        return cards.remove(card);
+    }
+
+    public boolean isComplete() {
+        return cards.size() == requiredCount;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getRequiredCount() {
+        return requiredCount;
+    }
+
+    public int getCurrentCount() {
+        return cards.size();
+    }
+
+    public List<Card> getCards() {
+        return new ArrayList<>(cards);
+    }
 }
